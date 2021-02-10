e2b881325a5e02e3d14a63724562a9bb27715c2a
jonathancierp <cierp.jonathan@gmail.com>
2021-02-10 18:06:20 +0100

    Feat : Add sass modules

---
+++ src/components/core/CoreHeader.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="core-header">
+
+	</div>
+</template>
+
+<script>
+	import { defineComponent } from "vue"
+
+	export default defineComponent({
+		name: "CoreHeader.vue",
+		setup: () => {
+
+		}
+	})
+</script>
