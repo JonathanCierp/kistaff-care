e2ba9df28acc12b5d086910d36a3413b68ab6244
jonathancierp <cierp.jonathan@gmail.com>
2021-02-10 19:15:28 +0100

    Feat : add vuex

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from "vue"
 import router from "./router/router"
+import store from "./store/store"
 import App from "./App.vue"
 import "./assets/css/app.scss"
 
 createApp(App)
     .use(router)
+    .use(store)
     .mount("#page-wrapper")
